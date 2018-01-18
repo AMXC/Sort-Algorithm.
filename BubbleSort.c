71eddde41b3d84482dcc8b58c4f687bd78ea0a09
AMXC <lovwfacus@163.com>
2018-01-18 10:51:24 +0800

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -4,7 +4,13 @@
 
 int main()
 {
--
-
+  int i;
+  int arr[] = {15,52,889,78,4,6,45};
+  BubbleSort(arr,sizeof(arr)/sizeof(arr[0]));
+  for(i = 0;i<sizeof(arr)/sizeof(arr[0]))
+  {
+    printf("%d  ",arr[i]);     
+  }
+  printf("\n");
   return 0;
 }
