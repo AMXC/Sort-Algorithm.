6cda34e5fa3ddd1eda8eba181f16832e66093f6f
AMXC <lovwfacus@163.com>
2018-01-18 10:55:20 +0800

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -2,6 +2,25 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+void BubbleSort(int *arr,int nLength)
+{
+  int i,j;
+ if(arr == NULL&&nLength<=0) return;
+  for(i = 0;i<nLength;i++)
+  {
+    for(j = 0;j<nLength;j++)
+    {
+     if(arr[j] > ar[j+1])
+     {
+        arr[j] = arr[j]^arr[j+1];
+        arr[j+1] = arr[j]^arr[j+1];
+        arr[j] =  arr[j]^arr[j+1];
+     }
+    }
+  }
+
+}
+
 int main()
 {
   int i;
